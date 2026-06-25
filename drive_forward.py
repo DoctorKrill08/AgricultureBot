91de48c9300031a6ec52adc4c42ab0885de0fce7
DoctorKrill08 <doctorkrill08@gmail.com>
2026-06-25 11:39:51 -0400

    works

--- drive_forward.py
+++ drive_forward.py
@@ -1,12 +1,12 @@
 from System.robot import *
 if __name__ == "__main__":
     Robot.turn_on()
-    RUN_TIME = 5
+    RUN_TIME = 3
     timer = Timer()
     time.sleep(.1)
     Robot.set_state(RobotState.AUTONOMOUS)
+    time.sleep(.1)
     while (timer.time_passed() < RUN_TIME):
-        print(timer.time_passed())
         Robot.joy_x = 0
         Robot.joy_y = 0.4
         Robot.update()
