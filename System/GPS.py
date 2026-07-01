dcf14b8ffd4dc6a6dccde044df8de4e83b69e32b
DoctorKrill08 <doctorkrill08@gmail.com>
2026-07-01 00:50:45 -0400

    getting somewhere

--- System/GPS.py
+++ System/GPS.py
@@ -4,7 +4,7 @@ from timer import Timer
 from enum import Enum
 import math
 import re
-from pyubx2 import UBXReader, UBX_PROTOCOL
+
 
 def miles_to_inches(miles):
     return miles * 63360
@@ -80,58 +80,117 @@ class CoordinateSystem(Enum):
             return coordinate
         return prev_coordinate + (coordinate - prev_coordinate)/num_of_coords
 
-class GPS:
 
 
-    INITIAL_POSITION_FOUND_READING_THRESHOLD = 50
-    connected = False
-    base_station_connected = False
+
+class GPSReceiver():
+    ROVER = "ROVER"
+    BASE = "BASE"
+
     NANO = "NANO"
     WINDOWS = "WINDOWS"
-    SERIAL_PORTS = {
-        NANO: '/dev/ttyACM1',
-        WINDOWS : 'COM8'
-    }
-    port = SERIAL_PORTS[NANO]
-    BAUD_RATE = 115200 # Default ZED-F9P rate is 115200
-    chip = None
-    PERIOD = 0.1 #Time inbetween readings
-    timer = Timer()
 
-    longitude = 0
-    latitude = 0
-
-    NS = Direction.NORTH #North or South
-    EW = Direction.WEST  #East or west
-
-    yaw = 0
+    BAUD_RATE = 115200
 
     POSITION_STREAM = '$GNGGA'
     RTK_STREAM = ''
 
+    ROVER_PORTS = {
+            NANO: '/dev/ttyACM1',
+            WINDOWS : 'COM8',
+    }
+    BASE_PORTS = {
+            NANO: '/dev/ttyACM2',
+            WINDOWS : 'COM10',
+    }
 
-    start_position = [0,0]
-    start_position_found = False
-    reading_count = 0
+    def __init__(self,type):
+        self.serial_ports = GPSReceiver.ROVER_PORTS
+        if (type == GPSReceiver.BASE):
+            self.serial_ports = GPSReceiver.BASE_PORTS
+            #TBA add RTK stream logic
+        self.type = type
+        self.connected = False
+        
+        self.serial = self.serial_ports[self.NANO]
 
+        self.longitude = 0
+        self.latitude = 0
+        self.quality = 0
 
-    local_grid = [0,0]
+        self.yaw = 0
 
-    def global_to_local():
-        return
-    def local_to_global():
-        return
+        self.target_stream = GPSReceiver.POSITION_STREAM
+
+    def start(self):
+        print(self.serial_ports)
+        self.connected = False
+        for key,value in self.serial_ports.items():
+            if (self.connected):
+                return
+            try:
+                self.serial = serial.Serial(value,  self.BAUD_RATE, timeout=1)
+                self.connected = True
+                print(f"Connected to {self.type} RTK receiver via {key}: {value}")
+            except:
+                pass
+
+    def read(self):
+        if (not  self.connected):
+            print(f"{self.type} NOT CONNECTED")
+            return
+        lines = self.serial.read_all().decode('utf-8', errors='ignore').strip()
+        lines = re.split(r'(\n)',lines)
+        print("--------")
+        for line in (lines):
+            if line.startswith(self.target_stream):
+                lat,lon,quality = GPSReceiver.parse_gps(line)
+                if (lat == None or lon == None or lat == "" or lon == ""):
+                    return
+                lat = CoordinateSystem.DDM_TO_DD(lat,CoordinateSystem.LATITUDE)
+                lon = CoordinateSystem.DDM_TO_DD(lon,CoordinateSystem.LONGITUDE)
+                self.start_position_found = True
+                self.latitude = lat
+                self.longitude = lon
+                self.quality = quality
+                print(self.status())
+    
+    def close(self):
+        if (self.connected):
+            self.serial.close()
+        self.connected = False
+
+    def status(self):
+        return f"{self.type},connected:{self.connected},latitude:{self.latitude},longitude:{self.longitude},quality:{self.quality}-{GPSReceiver.int_to_quality(self.quality)}"
+
+    @staticmethod
+    def int_to_quality(quality):
+        if (isinstance(quality,str)):
+            quality = int(quality)
+        if (quality == 1):
+            return "Standard GPS" #1 - 5 m
+        if (quality == 2):
+            return "Differential GPS" #1 - 3m
+        if (quality == 3):
+            return "PPS" #Government signal, should not be possible
+        if (quality == 4):
+            return "RTK Fixed" #Most accurate 1 - 2 cm
+        if (quality == 5):
+            return "RTK Float" #20 - 50cm
+        return "Invalid"
+
+    @staticmethod
     def parse_gps(line :str):
-        latitude = GPS.latitude
-        longitude = GPS.longitude
+        latitude = None
+        longitude = None
         type,_,line = line.partition(f',')
-        if (type == GPS.POSITION_STREAM):
+        if (type == GPSReceiver.POSITION_STREAM):
             time,_,line = line.partition(",")
             latitude,_,line = line.partition(",")
             north_south,_,line = line.partition(",")
             longitude,_,line = line.partition(",")
             east_west,_,line = line.partition(",")
-            quality,_,line = line.partition(",")
+            fix_quality,_,line = line.partition(",")
 
             """
             print("TYPE",type)
@@ -140,78 +199,61 @@ class GPS:
             print("N/S: ",north_south)
             print("LONGITUDE: ",longitude)
             print("E/W: ",east_west)
-            print("FIX QUALITY: ",quality)
+            print("FIX QUALITY: ",fix_quality)
             """
 
-            return latitude,longitude
+            return latitude,longitude,fix_quality
+class GPS:
+
+    rover = GPSReceiver(GPSReceiver.ROVER)
+    base = GPSReceiver(GPSReceiver.BASE)
 
 
+    PERIOD = 0.2 #Time inbetween readings
+    timer = Timer()
+
+    local_grid = [0,0]
+
+    def global_to_local():
+        return
+    def local_to_global():
+        return
+
+    def start():
+        GPS.rover.start()
+        GPS.base.start()
+    def close():
+        GPS.rover.close()
+        GPS.base.close()
     def status():
-        return f"START_POSITION_READY: {GPS.start_position_found} READING_COUNT: {GPS.reading_count}\n \
-            LAT: {GPS.latitude} LON: {GPS.longitude}\n \
-            START_LAT: {GPS.start_position[0]} START_LONG: {GPS.start_position[1]}\n \
-            DELTA_LAT: {GPS.local_grid[0]} DELTA_LON: {GPS.local_grid[1]}"
+        GPS.rover.status()
+        GPS.base.status()
+    def signal_base_to_rover():
+        waiting = GPS.base.serial.in_waiting
+        if waiting:
+            data = GPS.base.serial.read(waiting)
 
-    def connect_gps():
-        GPS.connected = False
-        GPS.start_position_found = False
-        GPS.start_position = []
-        for key,value in GPS.SERIAL_PORTS.items():
-            if (GPS.connected):
-                return
-            try:
-                GPS.chip = serial.Serial(value, GPS.BAUD_RATE, timeout=1)
-                print(f"Connected to ArduSimple RTK receiver via {key}: {value}")
-                GPS.port = key
-                GPS.connected = True
-                GPS.timer.reset()
-                GPS.position_stream_timer.reset()
-            except:
-                pass
-    def read():
-        if (not GPS.connected):
-            print("GPS NOT CONNECTED")
-            return
-        lines = GPS.chip.read_all().decode('utf-8', errors='ignore').strip()
-        lines = re.split(r'(\n)',lines)
-        print("--------")
-        for line in (lines):
-            if line.startswith(GPS.POSITION_STREAM):
-                lat,lon = GPS.parse_gps(line)
-                lat = CoordinateSystem.DDM_TO_DD(lat,CoordinateSystem.LATITUDE)
-                lon = CoordinateSystem.DDM_TO_DD(lon,CoordinateSystem.LONGITUDE)
-                if (GPS.reading_count < GPS.INITIAL_POSITION_FOUND_READING_THRESHOLD):
-                    GPS.latitude = CoordinateSystem.recursive_average(GPS.latitude,lat,GPS.reading_count)
-                    GPS.longitude = CoordinateSystem.recursive_average(GPS.longitude,lon,GPS.reading_count)
-                    GPS.start_position = [GPS.latitude,GPS.longitude]
-                    GPS.reading_count += 1
-                else:
-                    GPS.start_position_found = True
-                    GPS.latitude = lat
-                    GPS.longitude = lon
-                d_lat,d_lon,distance = CoordinateSystem.displacement([GPS.latitude,GPS.longitude],GPS.start_position)
-                GPS.local_grid = [d_lat,d_lon]
-                print("DISTANCE: ", distance)
-                print(GPS.status())
+            print(f"Forwarding {len(data)} bytes")
 
-
+            GPS.rover.serial.write(data)
+            GPS.rover.serial.flush()
+            print(f"Wrote {len(data)} bytes to rover")
     def update():
-        GPS.read()
+        GPS.rover.read()
+       # GPS.base.read()
+
+        if (GPS.rover.connected and GPS.base.connected):
+            GPS.signal_base_to_rover()
         if (GPS.timer.time_passed() < GPS.PERIOD):
             time.sleep(GPS.PERIOD - GPS.timer.time_passed())
         GPS.timer.reset()
         
-    def close():
-        if (GPS.connected):
-            GPS.chip.close()
-        GPS.connected = False
 
 
 #To run:
 #Windows: python -m System.GPS
 #Linux: python3 -m System.GPS (I think)
 if __name__ == "__main__":
-    GPS.connect_gps()
+    GPS.start()
     while True:
         GPS.update()
-    GPS.close()
