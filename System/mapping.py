66070933a82eebaba90534cf7a761e7ed4f35c1f
DoctorKrill08 <doctorkrill08@gmail.com>
2026-07-24 10:06:42 -0400

    Re added camera logic

--- System/mapping.py
+++ System/mapping.py
@@ -50,7 +50,30 @@ class Map:
         x = round_nearest(x,INCHES_PER_NODE)
         y = round_nearest(y,INCHES_PER_NODE)
         return x,y
-    def update(x,y,yaw,lidar_data,rotational_movement = 0):
+    def add_obstacle(horizontal,forward,x=0,y=0,yaw=0):
+        if (horizontal == None or forward == None):
+            return
+        d = math.sqrt((horizontal ** 2) + (forward ** 2))
+        d = d + CAMERA_DISTANCE_FROM_ROBOT
+        if d <= CAMERA_DISTANCE_FROM_ROBOT + CAMERA_MIN_DEPTH:
+            #what
+            return
+        if d >= Map.MAX_DISTANCE:
+            return
+        relative_angle = math.atan2(horizontal,forward)
+        if math.degrees(abs(relative_angle)) + 15 > CAMERA_HORIZONTAL_FOV / 2:
+            return
+
+        angle = add_angle(yaw,relative_angle)
+
+        x += d * math.cos(angle)
+        y += d * math.sin(angle)
+        x,y = Map.point_to_node(x,y)
+
+        node = Node(x,y,Node.OBSTACLE,raw_horizontal=horizontal,raw_forward=forward)
+        Map.visible_obstacles[node.id] = node
+        Map.nodes[node.id] = node
+    def update(x,y,yaw,lidar_data,camera_data = None,rotational_movement = 0):
         Map.calculate_visibility(x,y,yaw,rotational_movement)
         for point in lidar_data:
             if (point == None):
@@ -65,6 +88,12 @@ class Map:
                         continue
             node = Node(x,y,Node.OBSTACLE,raw_x=point[0],raw_y=point[1])
             Map.nodes[node.id] = node
+        for point in camera_data:
+            if (point == None):
+                continue
+            horizontal = point[0]
+            forward = point[1]
+            Map.add_obstacle(horizontal,forward,x,y,yaw)
             
     #Look at each obstacle node and determine its visibility
     #Run this after add obstacles
